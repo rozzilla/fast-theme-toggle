24b00053b53750c3559f3ee111ba1bb32e19cf28
Roberto Bianchi <roberto.bianki@gmail.com>
2021-09-18 17:37:40 +0200

    Change icon when extension is enabled

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -1,3 +1,27 @@
 chrome.browserAction.onClicked.addListener((tabs) =>
   chrome.tabs.sendMessage(tabs.id, "main-icon")
 );
+
+chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+  if (msg.fastThemeToggle) {
+    chrome.tabs.query(
+      { active: true, windowType: "normal", currentWindow: true },
+      (tab) => {
+        chrome.browserAction.setIcon({
+          path: "/src/images/icon128-enabled.png",
+          tabId: tab[0].id,
+        });
+      }
+    );
+  } else {
+    chrome.tabs.query(
+      { active: true, windowType: "normal", currentWindow: true },
+      (tab) => {
+        chrome.browserAction.setIcon({
+          path: "/src/images/icon128.png",
+          tabId: tab[0].id,
+        });
+      }
+    );
+  }
+});
--- src/scripts/content.js
+++ src/scripts/content.js
@@ -3,14 +3,19 @@ const main = document.querySelector("html").classList;
 
 const isThemeEnabled = () => localStorage.getItem(themeName) === "enabled";
 
-if (isThemeEnabled()) main.add(themeName);
+if (isThemeEnabled()) {
+  main.add(themeName);
+  chrome.runtime.sendMessage({ fastThemeToggle: true });
+}
 
 chrome.runtime.onMessage.addListener(() => {
   if (isThemeEnabled()) {
     main.remove(themeName);
     localStorage.removeItem(themeName);
+    chrome.runtime.sendMessage({ fastThemeToggle: false });
   } else {
     main.add(themeName);
     localStorage.setItem(themeName, "enabled");
+    chrome.runtime.sendMessage({ fastThemeToggle: true });
   }
 });
