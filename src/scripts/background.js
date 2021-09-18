eeaacca33d7670bec929ac59d9f9c4109c0c132a
Roberto Bianchi <roberto.bianki@gmail.com>
2021-09-18 17:50:27 +0200

    Refactor on background file

--- src/scripts/background.js
+++ src/scripts/background.js
@@ -1,27 +1,23 @@
+const pathImages = "/src/images";
+
+const changeExtensionIcon = (themeEnabled) => {
+  const icon = themeEnabled ? "icon128-enabled.png" : "icon128.png";
+
+  chrome.tabs.query(
+    { active: true, windowType: "normal", currentWindow: true },
+    (tab) => {
+      chrome.browserAction.setIcon({
+        path: `/src/images/${icon}`,
+        tabId: tab[0].id,
+      });
+    }
+  );
+};
+
 chrome.browserAction.onClicked.addListener((tabs) =>
   chrome.tabs.sendMessage(tabs.id, "main-icon")
 );
 
-chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-  if (msg.fastThemeToggle) {
-    chrome.tabs.query(
-      { active: true, windowType: "normal", currentWindow: true },
-      (tab) => {
-        chrome.browserAction.setIcon({
-          path: "/src/images/icon128-enabled.png",
-          tabId: tab[0].id,
-        });
-      }
-    );
-  } else {
-    chrome.tabs.query(
-      { active: true, windowType: "normal", currentWindow: true },
-      (tab) => {
-        chrome.browserAction.setIcon({
-          path: "/src/images/icon128.png",
-          tabId: tab[0].id,
-        });
-      }
-    );
-  }
-});
+chrome.runtime.onMessage.addListener((msg) =>
+  changeExtensionIcon(msg.fastThemeToggle)
+);
