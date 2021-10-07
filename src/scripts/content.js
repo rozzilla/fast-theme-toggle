898439b618260162177ce5f342b74d28d5bfd68e
Roberto Bianchi <roberto.bianki@gmail.com>
2021-10-07 21:32:27 +0200

    Simplify logic with event mapping lookup

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -1,16 +1,12 @@
 (async () => {
   const { isThemeEnabled, applyTheme } = await import("./helper.js");
 
-  chrome.runtime.onMessage.addListener((message) => {
-    switch (message) {
-      case "tab-active":
-        applyTheme(isThemeEnabled());
-        break;
-      case "tab-click":
-        applyTheme(!isThemeEnabled());
-        break;
-      default:
-        console.warn("Unhandled Chrome tab message", message);
-    }
-  });
+  const eventMapping = {
+    "tab-active": isThemeEnabled(),
+    "tab-click": !isThemeEnabled(),
+  };
+
+  chrome.runtime.onMessage.addListener((message) =>
+    applyTheme(eventMapping[message])
+  );
 })();
